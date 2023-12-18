275b175e7c8ca64c74ca559255a5b6f6f16a0d18
viniciszs <114860862+viniciszs@users.noreply.github.com>
2023-12-17 21:18:22 -0300

    deploy

--- src/router/revenue.ts
+++ src/router/revenue.ts
@@ -8,3 +8,14 @@ router.get('/', async (req, res) => {
   const revenue = await prisma.revenue.findMany()
   res.send(revenue)
 })
+
+router.post('/', async (req, res) => {
+  const {month, revenue} = req.body
+  await prisma.revenue.create({
+    data: {
+      month,
+      revenue
+    }
+  })
+  res.send('create')
+})
